47c017dfdc55ffa6c522cfdae963fb6269dbf5ff
Yuri Schimke <yuri@schimke.ee>
2018-01-17 07:57:11 +0000

    test versions of android

--- app/src/main/kotlin/com/squareup/okhttptestapp/model/ConfigSpec.kt
+++ app/src/main/kotlin/com/squareup/okhttptestapp/model/ConfigSpec.kt
@@ -1,5 +1,6 @@
 package com.squareup.okhttptestapp.model
 
+import com.squareup.okhttptestapp.network.Cipher
 import okhttp3.CipherSuite
 import okhttp3.ConnectionSpec
 import okhttp3.TlsVersion
@@ -30,4 +31,42 @@ object Example : ConfigSpec() {
   override fun toString(): String = "Example"
 }
 
-val allSpecs = listOf(Modern, Compatible, Example)
+class AndroidSpec(val version: Int, val enabledTlsVersions: List<TlsVersion>,
+    val supportedTlsVersions: List<TlsVersion>, val enabled: List<String>,
+    val supported: List<String>) : ConfigSpec() {
+
+  // TODO warning for modes not supported on running version
+  override fun connectionSpec(): ConnectionSpec = ConnectionSpec.Builder(
+      ConnectionSpec.COMPATIBLE_TLS)
+      .tlsVersions(*enabledTlsVersions.toTypedArray())
+      .cipherSuites(*enabled.toTypedArray())
+      .supportsTlsExtensions(true)
+      .build()
+
+  override fun toString(): String = "Android " + version
+}
+
+val androidSpecs: List<AndroidSpec> = (27 downTo 1).map { version ->
+  val enabledTls = when (version) {
+    in 1..15 -> listOf(TlsVersion.SSL_3_0, TlsVersion.TLS_1_0)
+    in 16..22 -> listOf(TlsVersion.SSL_3_0, TlsVersion.TLS_1_0, TlsVersion.TLS_1_1,
+        TlsVersion.TLS_1_2)
+    in 23..27 -> listOf(TlsVersion.TLS_1_0, TlsVersion.TLS_1_1, TlsVersion.TLS_1_2)
+    else -> TODO()
+  }
+
+  val supportedTls = when (version) {
+    in 1..15 -> listOf(TlsVersion.SSL_3_0, TlsVersion.TLS_1_0)
+    in 16..25 -> listOf(TlsVersion.SSL_3_0, TlsVersion.TLS_1_0, TlsVersion.TLS_1_1,
+        TlsVersion.TLS_1_2)
+    in 26..27 -> listOf(TlsVersion.TLS_1_0, TlsVersion.TLS_1_1, TlsVersion.TLS_1_2)
+    else -> TODO()
+  }
+
+  val enabled = Cipher.values().filter { it.enabledRange != null && version in it.enabledRange }.map { it.name }
+  val supported = Cipher.values().filter { it.supportedRange != null && version in it.supportedRange }.map { it.name }
+
+  AndroidSpec(version, enabledTls, supportedTls, enabled, supported)
+}
+
+val allSpecs: List<ConfigSpec> = listOf(Modern, Compatible, Example) + androidSpecs
